a763a55502660664d92bb21d26121e3de363b6ff
Dongmin Lee <medi8600@gmail.com>
2016-05-02 15:23:23 -0500

    login logout and index fixed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,16 @@
 class EventsController < ApplicationController
+  def show
+    @event = current_user.events.find_by(params[:id])
+  end
 
   def index
-    @event = Event.find_by(@user)
+    @events = Event.all
+    @users = User.all
   end
 
   def new
     @event = Event.new
+    @user = current_user
   end
 
   def create
@@ -14,5 +19,22 @@ class EventsController < ApplicationController
     redirect_to user_path @user
   end
 
+  def edit
+    @event = current_user.events.find_by(params[:id])
+  end
+
+  def update
+    @user = current_user
+    event = current_user.events.find_by(params[:id])
+    event.update(title: params[:event][:title], date: params[:event][:date], time: params[:event][:time], location: params[:event][:location])
+    redirect_to user_path @user
+  end
+
+  def destroy
+    @user = current_user
+    event = current_user.events.find_by(params[:id])
+    event.destroy
+    redirect_to user_path @user
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'events#index'
   get "/events", to: "events#index", as: "events"
   get "/events/new", to: "events#new", as: "new_event"
-  # get "/events/:id", to: "events#show", as: "tool"
+  get "/events/:id", to: "events#show", as: "event"
   post "/events", to: "events#create"
-  # get "/events/:id/edit", to: "events#edit", as: "edit_tool"
-  # patch "/events/:id", to: "events#update"
-  # put "/events/:id", to: "events#update"
-  # delete "/events/:id", to: "events#destroy"
+  get "/events/:id/edit", to: "events#edit", as: "edit_event"
+  patch "/events/:id", to: "events#update"
+  put "/events/:id", to: "events#update"
+  delete "/events/:id", to: "events#destroy"
   resources :users
 
   get 'login' => 'sessions#new'
